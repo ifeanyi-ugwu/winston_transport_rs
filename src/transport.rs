840874a9923565bcd47da122086d1c1fdfce213c
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-10-14 07:14:18 +0100

    added default implementations for ghe get_level and get_format methods in the Transport trait

--- src/transport.rs
+++ src/transport.rs
@@ -4,8 +4,12 @@ use std::any::Any;
 
 pub trait Transport: Any + Send + Sync {
     fn log(&self, info: LogInfo);
-    fn get_level(&self) -> Option<&String>;
-    fn get_format(&self) -> Option<&Format>;
+    fn get_level(&self) -> Option<&String> {
+        None
+    }
+    fn get_format(&self) -> Option<&Format> {
+        None
+    }
     fn as_any(&self) -> &dyn Any;
     fn as_queryable(&self) -> Option<&dyn Queryable> {
         None
