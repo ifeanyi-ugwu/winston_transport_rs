5b06a062bc10f60c279a5160d4ac507cec4a4327
ifeanyi-valentine <urielifeanyi@gmail.com>
2025-03-24 07:30:56 +0100

    feat: export `LogQuery` and `Format` so that authors of transport crates wont have to add `logform` as a dependency just to reference them

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ mod transport;
 mod writer_transport;
 
 pub use log_query::{LogQuery, Order};
+pub use logform::{Format, LogInfo};
 pub use transport::Transport;
 pub use writer_transport::WriterTransport;
